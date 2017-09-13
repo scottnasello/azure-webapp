71cbb885d121afb79ee6e3b5a19aa90eb7f9eead
Nasello, Scott <nasello.scott@gmail.com>
2017-09-12 20:01:22 -0700

    webapp changes

--- deploy.ps1
+++ deploy.ps1
@@ -1,39 +1,20 @@
-$gitrepo="https://github.com/Azure-Samples/app-service-web-dotnet-get-started.git"
-$webappname="mywebapp$(Get-Random)"
+$gitrepo="https://github.com/scottnasello/azure-webapp.git"
+$webappname="rtd-webapp"
 $location="West US"
-$resourcegroup="rtd-webapp"
-
-#Remove Resource Group
-Remove-AzureRmResourceGroup -Name $resourcegroup -Force
 
 # Create a resource group.
-New-AzureRmResourceGroup -Name $resourcegroup -Location $location
+New-AzureRmResourceGroup -Name $webappname -Location $location
 
-# Create an App Service plan in `Free` tier.
-New-AzureRmAppServicePlan -Name $webappname -Location $location `
--ResourceGroupName $resourcegroup -Tier Free
+# Create an App Service plan in Free tier.
+New-AzureRmAppServicePlan -Name $webappname -Location $location -ResourceGroupName $webappname -Tier Free
 
 # Create a web app.
-New-AzureRmWebApp -Name $webappname -Location $location -AppServicePlan $webappname `
--ResourceGroupName $resourcegroup
+New-AzureRmWebApp -Name $webappname -Location $location -AppServicePlan $webappname -ResourceGroupName $webappname
 
 # Configure GitHub deployment from your GitHub repo and deploy once.
 $PropertiesObject = @{
     repoUrl = "$gitrepo";
     branch = "master";
+    isManualIntegration = "true";
 }
-
-Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName $resourcegroup `
--ResourceType Microsoft.Web/sites/config -ResourceName $webappname/web `
--ApiVersion 2015-08-01 -Force
-
-# Get app-level deployment credentials
-$xml = [xml](Get-AzureRmWebAppPublishingProfile -Name $webappname -ResourceGroupName $resourcegroup `
--OutputFile null)
-$username = $xml.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userName").value
-$password = $xml.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userPWD").value
-
-# Add the Azure remote to your local Git respository and push your code
-#### This method saves your password in the git remote. You can use a Git credential manager to secure your password instead.
-git remote add azure "https://${username}:$password@$webappname.scm.azurewebsites.net"
-git push azure master
+Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName $webappname -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $webappname/web -ApiVersion 2015-08-01 -Force
